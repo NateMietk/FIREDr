0cbd531ada5310244d64889958ac1f5598f3f0f1
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-03-21 11:47:59 -0600

    updated URL

--- R/download_tiles.R
+++ R/download_tiles.R
@@ -12,7 +12,7 @@
 #
 # Future capabilities: Add custom date ranges for download
 
-download_tiles <- function(tiles, url = "ftp://fire:burnt@fuoco.geog.umd.edu/MCD64A1/C6/", u_p = "fire:burnt", out_dir) {
+download_tiles <- function(tiles, url = "ftp://fire:burnt@fuoco.geog.umd.edu/gfed4/MCD64A1/C6/", u_p = "fire:burnt", out_dir) {
 
   require(tidyverse)
 
